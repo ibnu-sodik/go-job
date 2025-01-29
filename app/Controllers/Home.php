e4cf867cdfb987f9b4959592dac0f25faf4b1aa6
ibnu-sodik <ibnusodik049@gmail.com>
2025-01-29 14:27:33 +0700

    Creating Master Template and Homepage

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        // return view('welcome_message');
-        return "Welcome to Go-Job Platform by Go-Blog Informatika";
+        return view('home');
+        // return "Welcome to Go-Job Platform by Go-Blog Informatika";
     }
 }
