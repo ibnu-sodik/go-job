bb7c44ad9de905e9619d007b71c48c3eff054fcb
ibnu-sodik <ibnusodik049@gmail.com>
2025-01-29 13:43:54 +0700

    Creating Authentication

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'dixos252@gmail.com';
+    public string $fromName   = 'GoBlog';
     public string $recipients = '';
 
     /**
